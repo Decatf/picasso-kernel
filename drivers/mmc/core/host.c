06220d237471512d6bc4dd9408ee3d91972af672
ryang <decatf@gmail.com>
2017-07-23 16:21:40 -0400

    HACK: mmc: core: host: export symbols for bcmdhd module

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -168,6 +168,7 @@ void mmc_host_clk_hold(struct mmc_host *host)
 	spin_unlock_irqrestore(&host->clk_lock, flags);
 	mutex_unlock(&host->clk_gate_mutex);
 }
+EXPORT_SYMBOL(mmc_host_clk_hold);
 
 /**
  *	mmc_host_may_gate_card - check if this card may be gated
@@ -209,6 +210,7 @@ void mmc_host_clk_release(struct mmc_host *host)
 				      msecs_to_jiffies(host->clkgate_delay));
 	spin_unlock_irqrestore(&host->clk_lock, flags);
 }
+EXPORT_SYMBOL(mmc_host_clk_release);
 
 /**
  *	mmc_host_clk_rate - get current clock frequency setting
@@ -229,6 +231,7 @@ unsigned int mmc_host_clk_rate(struct mmc_host *host)
 	spin_unlock_irqrestore(&host->clk_lock, flags);
 	return freq;
 }
+EXPORT_SYMBOL(mmc_host_clk_rate);
 
 /**
  *	mmc_host_clk_init - set up clock gating code
